faeabcfa8c7cedf42ed95e8b2ec85c73ec99ef3b
Sarthak <sarthakmonga2001@gmail.com>
2025-06-14 21:22:50 +0530

    changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import os
 
 app = FastAPI()
 
